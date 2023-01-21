9dfa67deeefbbbe244b1da4dc724a78295ed4a4a
AXO <77832984+AqsoLath@users.noreply.github.com>
2023-01-21 02:03:15 +0000

    benerin verifikasi recaptcha

--- App_Data/Models/all.generated.cs
+++ App_Data/Models/all.generated.cs
@@ -7,7 +7,7 @@ using  Umbraco.Core.Models.PublishedContent;
 using  Umbraco.Web;
 using  Umbraco.ModelsBuilder.Embedded;
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "f32e2e6f218a4cd8")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
