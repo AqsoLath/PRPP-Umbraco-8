b9507814367d370c69b1e1c605088856b3633462
AXO <77832984+AqsoLath@users.noreply.github.com>
2023-02-02 18:45:45 +0700

    benerin recaptcha, benerin pagination di home main slider

--- App_Code/Controllers/ContactController.cs
+++ App_Code/Controllers/ContactController.cs
@@ -6,6 +6,14 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
+// using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.IO;
+
+
+
 using Umbraco.Core;
 using Umbraco.Core.Models;
 using Umbraco.Core.Services;
@@ -16,6 +24,7 @@ using Umbraco.Web.Mvc;
 using Newtonsoft.Json;
 
 using App_Code.Models;
+
 
 namespace App_Code.Controllers
 {
@@ -43,6 +52,16 @@ namespace App_Code.Controllers
                     throw new Exception();
                 }
 
+                string response = model.Recaptcha;
+
+                var valid = Validate(response);
+
+                if (valid == "false")
+                {
+                    Console.WriteLine("Authentification False");
+                    throw new Exception();
+                }
+
                 var parentGuid = Guid.Parse("7e854621-8ba2-4539-8f07-a09184ecf403");
                 var contactMessage = _contentService.Create(System.Guid.NewGuid().ToString(), parentGuid, "contactMessages");
 
@@ -64,5 +83,35 @@ namespace App_Code.Controllers
                 return CurrentUmbracoPage();
             }
         }
+
+        public static string Validate(string EncodedResponse)
+        {
+            var client = new System.Net.WebClient();
+
+            string PrivateKey = "6LdcQs0iAAAAAIeTZb7l3-j0VJZy6yAvXqXpuyNc";
+
+            var GoogleReply = client.DownloadString(string.Format("https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}", PrivateKey, EncodedResponse));
+
+            var captchaResponse = Newtonsoft.Json.JsonConvert.DeserializeObject<ContactController>(GoogleReply);
+
+            return captchaResponse.Success.ToLower();
+        }
+
+        [JsonProperty("success")]
+        public string Success
+        {
+            get { return m_Success; }
+            set { m_Success = value; }
+        }
+
+        private string m_Success;
+        [JsonProperty("error-codes")]
+        public List<string> ErrorCodes
+        {
+            get { return m_ErrorCodes; }
+            set { m_ErrorCodes = value; }
+        }
+
+        private List<string> m_ErrorCodes;
     }
 }
