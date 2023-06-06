c9b706a2c92229c9c281be16c2e90b58d66e7d00
dev-jenaka <developer.jenaka.co.id>
2023-06-06 10:48:52 +0000

    commit server

--- App_Data/Models/models.generated.cs
+++ App_Data/Models/models.generated.cs
@@ -18,7 +18,7 @@ using Umbraco.Web;
 using Umbraco.ModelsBuilder.Embedded;
 
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "12fbf151de5e0b1d")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.5")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
 
 namespace Umbraco.Web.PublishedModels
 {
